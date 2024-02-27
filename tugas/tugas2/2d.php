a0a9164788b5a5006b71a420b8264caacd66bf8f
sachrulakbar <145645888+sachrulakbar@users.noreply.github.com>
2024-02-27 13:35:39 +0700

    tugas 2

--- tugas/tugas2/2d.php
+++ tugas/tugas2/2d.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 2d Kresna Satria Dewantoro</title>
+    <title>Tugas 2d Moch Sachrul Akbar R</title>
     <style>
         .flex {
             display: flex;
