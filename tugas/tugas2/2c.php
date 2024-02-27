9e422335893f42feba5660adaeec60d72e28ec78
sachrulakbar <145645888+sachrulakbar@users.noreply.github.com>
2024-02-27 13:36:25 +0700

    tugas 2

--- tugas/tugas2/2c.php
+++ tugas/tugas2/2c.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 2c Moch Sachrul Akbar R</title>
+    <title>Tugas 2c Moch Sachrul Akbar Ramadhan</title>
     <style>
         .flex {
             display: flex;
